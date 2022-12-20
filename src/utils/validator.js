f4e42f20f4274bf0a3a30a2626b2bce2e4424e48
AbhijitRadke <radkeabhi27@gmail.com>
2022-12-20 08:58:53 +0530

    v2

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+
 
 const isEmpty = function (value) {
     if (typeof value === "undefined" || value === null) return false;
@@ -11,29 +11,31 @@ const isValidBody = function (object) {
 }
 
 const isValidName = function (name) {
+    if (typeof name === "undefined" || name === null) return false;
+    if (typeof name === "string" && name.trim().length === 0) return false;
     const nameRegex = /^[a-zA-Z ]+$/;
     return nameRegex.test(name);
 }
 
 const isValidPhone = function (num) {
+    if (typeof num === "undefined" || num === null) return false;
+    if (typeof num === "string" && num.trim().length === 0) return false;
     const reg = /^[0-9]{10}$/;
     return reg.test(num);
 }
 
 const isValidEmail = function (email) {
-    const emailRegex =
-        /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
+    if (typeof email === "undefined" || email === null) return false;
+    if (typeof email === "string" && email.trim().length === 0) return false;
+    const emailRegex = /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
     return emailRegex.test(email);
 }
 
-
-
 const isValidpincode = function (pincode) {
+    if (typeof pincode !== "Nunber" ) return false;
     const pincodeRegex = /^[1-9][0-9]{5}$/;
     return pincodeRegex.test(pincode);
 };
-
-
 
 
 module.exports = { isEmpty, isValidName, isValidEmail, isValidPhone, isValidBody, isValidpincode, }
