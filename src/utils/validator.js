0c455a6c0876bc2cae4eb7dec659925cad975898
nitu <nitujaiswal8271@gmail.com>
2022-12-21 16:32:02 +0530

    productmodel done

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -11,27 +11,33 @@ const isValidBody = function (object) {
 }
 
 const isValidName = function (name) {
+    if (typeof name === "undefined" || name === null) return false;
+    if (typeof name === "string" && name.trim().length === 0) return false;
     const nameRegex = /^[a-zA-Z ]+$/;
     return nameRegex.test(name);
 }
 
+
 const isValidPhone = function (num) {
+    if (typeof num === "undefined" || num === null) return false;
+    if (typeof num === "string" && num.trim().length === 0) return false;
     const reg = /^[0-9]{10}$/;
     return reg.test(num);
 }
-
 const isValidEmail = function (email) {
-    const emailRegex =
-        /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
+    if (typeof email === "undefined" || email === null) return false;
+    if (typeof email === "string" && email.trim().length === 0) return false;
+    const emailRegex = /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
     return emailRegex.test(email);
 }
 
-
-
 const isValidpincode = function (pincode) {
-    const pincodeRegex = /^[1-9][0-9]{5}$/;
-    return pincodeRegex.test(pincode);
+    
+    const reg = /^[0-9]{6}$/;
+    return reg.test(String(pincode));
 };
+
+
 
 const isValidInstallment = function isInteger(value) {
     if (value < 0) return false
@@ -47,18 +53,17 @@ const isvalidQuantity = function isInteger(value) {
     if (isNaN(Number(value))) return false
     if (value % 1 == 0) return true
 }
-//const isValidprice = function (price) {
-   // if (typeof price !== "Number" ) return false;
-    //const priceRegex = /^(\d{1,2})(,\d{2})*(,\d{1,3}){1}(\.\d{1,})?$/g;
-    //return priceRegex.test(price);
-//}
 const isvalidPrice = function (price) {
     return /^\d{0,8}(\.\d{1,2})?$/.test(String(price));
   };
   const isvalidSize = function (size) {
     return ["S", "XS", "M", "X", "L", "XXL", "XL"].includes(size);
   };
+  const isVaildPass = function (str) {
+    const re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,15}$/;
+    return re.test(str);
+}
   
-module.exports = { isvalidSize,isvalidPrice, isEmpty,isValidName, isValidEmail, isValidPhone, isValidBody, isValidpincode,isValidInstallment,isValidObjectId ,isvalidQuantity}
+module.exports = { isVaildPass,isvalidSize,isvalidPrice, isEmpty,isValidName, isValidEmail, isValidPhone, isValidBody, isValidpincode,isValidInstallment,isValidObjectId ,isvalidQuantity}
 
 
